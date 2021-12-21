192f4acd79a3481740e062c4b9f10fb4bc4724f0
Shubo Chao <shubochao@gmail.com>
2021-12-21 21:12:16 +0800

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,21 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
   console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
-      {data.allFile.nodes.map(node => {
+      {data.allMdx.nodes.map(node => {
         return (
-          <li key={node.name}>
-            {node.name}
-          </li>
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
         )
       })}
     </Layout>
@@ -18,10 +23,15 @@ const BlogPage = ({ data }) => {
 }
 
 export const query = graphql`
-  query MyQuery {
-    allFile {
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
